d3e4adb6a6608786155e27efa54b4ecba05325af
Athaya Naufal Brasta <athayanaufalbrasta@gmail.com>
2026-08-06 17:02:46 +0700

    feat(Harga trucking) : refactoring, export & report background

--- src/modules/hargatrucking/dto/create-hargatrucking.dto.ts
+++ src/modules/hargatrucking/dto/create-hargatrucking.dto.ts
@@ -1,24 +1,27 @@
 import { z } from 'zod';
 
 export const CreateHargatruckingSchema = z.object({
-  tujuankapal_id: z.string().min(1, { message: 'TUJUAN KAPAL ID is required' }),
+  tarifdetail_id: z.string().nullable().optional(),
+  tarifdetail_text: z.string().nullable().optional(),
+
+  tujuankapal_id: z.string().min(1, { message: 'Tujuan Kapal is required' }),
   tujuankapal_text: z.string().nullable().optional(),
 
-  emkl_id: z.string().min(1, { message: 'emkl id is required' }),
+  emkl_id: z.string().min(1, { message: 'EMKL is required' }),
   emkl_text: z.string().nullable().optional(),
 
-  keterangan: z.string().trim().min(1, { message: 'KETERANGAN is required' }),
+  keterangan: z.string().trim().nullable().optional(),
 
-  container_id: z.string().min(1, { message: 'Container id is required' }),
+  container_id: z.string().min(1, { message: 'Container is required' }),
   container_text: z.string().nullable().optional(),
 
   jenisorder_id: z.string().min(1, { message: 'Jenis Orderan is required' }),
-  jenisorderan_text: z.string().nullable().optional(),
+  jenisorder_text: z.string().nullable().optional(),
 
   nominal: z.string().min(1, { message: 'Nominal is required' }),
 
   statusaktif: z.string().min(1, { message: 'status aktif is required' }),
-  text: z.string().nullable().optional(),
+  statusaktif_text: z.string().nullable().optional(),
 
   info: z.string().nullable().optional(),
   modifiedby: z.string().nullable().optional(),
